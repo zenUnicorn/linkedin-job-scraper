ad830bf61344c762f4c03c44471f184df46c6999
shittu olumide <40125098+zenUnicorn@users.noreply.github.com>
2022-05-12 13:58:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from linkedin_jobs_scraper.filters import RemoteFilters
 
 logging.basicConfig(level = logging.INFO)
 
+
 def scdata(data: EventData):
   print('[SCDATA]', data.title, data.company, data.company_link,data.date, 
   data.link, data.insights, len(data.description))
@@ -19,3 +20,23 @@ def err(error):
 def end():
   print('[END]')
   
+  
+  
+data_scraper = LinkedinScraper(
+  #custom chrome executable path
+  chrome_executable_path = None,
+
+  #custom chrome options
+  chrome_options = None,
+
+  #overribes headless mode only if chrome_options ia None
+  headless = True,
+
+  #how many threads will be spawned to run queries concurrently
+  max_workers = 1,
+  
+  #slow down the scraper to avoid "Too many requests 429" errors
+  slow_mo = 1
+)
+
+
