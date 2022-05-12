491518e434ad2eb9d1ef0d8146098cf53bae5b44
shittu olumide <40125098+zenUnicorn@users.noreply.github.com>
2022-05-12 13:57:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ from linkedin_jobs_scraper.query import Query, QueryOptions, QueryFilters
 from linkedin_jobs_scraper.filters import RelevanceFilters, TimeFilters
 from linkedin_jobs_scraper.filters import TypeFilters, ExperienceLevelFilters
 from linkedin_jobs_scraper.filters import RemoteFilters
+
+
+logging.basicConfig(level = logging.INFO)
+
+def scdata(data: EventData):
+  print('[SCDATA]', data.title, data.company, data.company_link,data.date, 
+  data.link, data.insights, len(data.description))
+
+def err(error):
+  print('[ERROR]', error)
+
+def end():
+  print('[END]')
+  
