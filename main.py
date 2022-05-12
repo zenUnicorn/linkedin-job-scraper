7b08dfc8a210904ffbdcb91d518045033021ed59
shittu olumide <40125098+zenUnicorn@users.noreply.github.com>
2022-05-12 13:56:03 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import logging
+from linkedin_jobs_scraper import LinkedinScraper
+from linkedin_jobs_scraper.events import Events, EventData
+from linkedin_jobs_scraper.query import Query, QueryOptions, QueryFilters
+from linkedin_jobs_scraper.filters import RelevanceFilters, TimeFilters
+from linkedin_jobs_scraper.filters import TypeFilters, ExperienceLevelFilters
+from linkedin_jobs_scraper.filters import RemoteFilters
