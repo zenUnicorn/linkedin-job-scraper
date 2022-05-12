7812b00903b97ae1e72aaf96ab5eeb11a2c697c6
shittu olumide <40125098+zenUnicorn@users.noreply.github.com>
2022-05-12 15:20:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-import logging
+#import the libraries
 from linkedin_jobs_scraper import LinkedinScraper
-from linkedin_jobs_scraper.events import Events, EventData
-from linkedin_jobs_scraper.query import Query, QueryOptions, QueryFilters
-from linkedin_jobs_scraper.filters import RelevanceFilters, TimeFilters
-from linkedin_jobs_scraper.filters import TypeFilters, ExperienceLevelFilters
+from linkedin_jobs_scraper.events import Events
+from linkedin_jobs_scraper.events import EventData
+from linkedin_jobs_scraper.query import Query
+from linkedin_jobs_scraper.query import QueryOptions
+from linkedin_jobs_scraper.query import QueryFilters
+from linkedin_jobs_scraper.filters import RelevanceFilters
+from linkedin_jobs_scraper.filters import TimeFilters
+from linkedin_jobs_scraper.filters import TypeFilters
+from linkedin_jobs_scraper.filters import ExperienceLevelFilters
 from linkedin_jobs_scraper.filters import RemoteFilters
+import logging
 
-
+#Changing your root logger level
 logging.basicConfig(level = logging.INFO)
 
 
-def scdata(data: EventData):
-  print('[SCDATA]', data.title, data.company, data.company_link,data.date, 
-  data.link, data.insights, len(data.description))
-
-def err(error):
-  print('[ERROR]', error)
-
-def end():
-  print('[END]')
-  
-  
-  
 data_scraper = LinkedinScraper(
   #custom chrome executable path
   chrome_executable_path = None,
@@ -39,40 +33,60 @@ data_scraper = LinkedinScraper(
   slow_mo = 1
 )
 
+# Let's create the functions that grabs the data from the LinkedIn job page.
+def scdata(info: EventInfo):
+  print('[SCDATA]', info.title, info.link, info.company, info.company_link, info.company_img_link, info.description 
+   info.date, info.place)
 
-#add event listeners
+def err(error):
+  print('[ERR]', error)
+
+def end():
+  print('[END]')
+  
+  
+ 
+#The Event listeners should be added.
 data_scraper.on(Events.DATA, scdata)
 data_scraper.on(Events.ERROR, err)
 data_scraper.on(Events.END, end)
 
 
 
-queries = [
+variables = [
   Query(
     options = QueryOptions(
-      optimize = True, #blocks requests for sources like images and stylesheet
-      limit = 27 #this limits the number of jobs to scrape
+      #blocks requests for sources like images and stylesheet
+      optimize = True, 
+      #this limits the number of jobs to scrape
+      limit = 10 
     )
   ),
   Query(
     query = 'Designer',
     options = QueryOptions(
-      locations = ["United Kingdom"],
       optimize = False,
-      apply_link = True, #try to extract apply link
-      limit = 5,
+      apply_link = True,
+      locations = ["United Kingdom"],
+      limit = 10,
       filters = QueryFilters(
         company_jobs_url='https://www.linkedin.com/jobs/search?keywords=Designers&location=United%20Kingdom&0=&position=1&pageNum=0',
-        relevance = RelevanceFilters.RECENT,
-        time = TimeFilters.MONTH,
-        type = [TypeFilters.FULL_TIME, TypeFilters.INTERNSHIP],
-        expereice = None,
+        relevance = RelevanceFilters.RELEVANT,
+        time = TimeFilters.ANY,
+        type = [
+          TypeFilters.FULL_TIME,
+          TypeFilters.ASSOCIATE,
+          TypeFilters.DIRECTOR
+        ],
+        experience=[
+          ExperienceLevelFilters.ASSOCIATE, 
+          ExperienceLevelFilters.MID_SENIOR
+        ]
       )
     )
   ),
 ]
 
-
-
-data_scraper.run(queries)
+#run the linkedIn Scrapper
+data_scraper.run(variables)
 
